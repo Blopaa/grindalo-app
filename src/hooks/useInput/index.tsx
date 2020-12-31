0dfa4ac01d55ceec735c400d1c912fa83a942b6f
Pablo Sabater <pabloosabaterr@gmail.com>
2020-12-31 20:36:44 +0100

    feat: hook useInput

---
+++ src/hooks/useInput/index.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+interface IinitialState {
+    values?: string;
+}
+
+const useInput = (initialState: IinitialState = {}): any[] => {
+    const [state, setState] = useState(initialState)
+
+    const handleInputChange = ({target}: any): void => {
+        if(!target) throw new Error("no target sent");
+        setState({
+            ...state,
+            [target.name]: target.value
+        })
+    }
+
+    return [state as IinitialState, handleInputChange as () => void]
+}
+
+export default useInput
