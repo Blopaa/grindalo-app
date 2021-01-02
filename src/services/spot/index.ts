42906e1313310d5aff274f10334741ed8360edf2
Pablo Sabater <pabloosabaterr@gmail.com>
2021-01-02 21:49:50 +0100

    feat: getSpots services

---
+++ src/services/spot/index.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export const getSpots = async (token: string) => {
+  let res;
+  try {
+    const { data, status } = await axios.get(
+      `${process.env.REACT_APP_API_URI}/api/spots`,
+      {
+        headers: { 'auth-token': token },
+      }
+    );
+    res = { data, status };
+  } catch (err) {
+    const { status } = err.response;
+    const { message } = err.response.data;
+    res = { status, message };
+  } finally {
+    return res;
+  }
+};
