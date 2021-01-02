15367784a5349bca2d3e4495e73b296b76b591aa
Pablo Sabater <pabloosabaterr@gmail.com>
2021-01-02 21:24:45 +0100

    feat: giveLike service

--- src/services/user/index.tsx
+++ src/services/user/index.tsx
@@ -1,15 +1,39 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const getLikedPost = async (token: string) => {
-    let res;
-    try {
-        const {data, status} = await axios.get(`${process.env.REACT_APP_API_URI}/api/getliked`, {headers: {"auth-token": token}})
-        res = {data, status}
-    } catch (err) {
-        const {status} = err.response;
-        const {message} = err.response.data;
-        res = {status, message}
-    }finally{
-        return res
-    }
-}
+  let res;
+  try {
+    const {
+      data,
+      status,
+    } = await axios.get(`${process.env.REACT_APP_API_URI}/api/getliked`, {
+      headers: { 'auth-token': token },
+    });
+    res = { data, status };
+  } catch (err) {
+    const { status } = err.response;
+    const { message } = err.response.data;
+    res = { status, message };
+  } finally {
+    return res;
+  }
+};
+
+export const giveLike = async (spotId: number, token: string) => {
+  let res;
+  try {
+    const {
+      data,
+      status,
+    } = await axios.get(`${process.env.REACT_APP_API_URI}/api/likespot/${spotId}`, {
+      headers: { 'auth-token': token },
+    });
+    res = { data, status };
+  } catch (err) {
+    const { status } = err.response;
+    const { message } = err.response.data;
+    res = { status, message };
+  } finally {
+    return res;
+  }
+};
