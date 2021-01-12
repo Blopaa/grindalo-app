217b5b0e608e119ac2201973b4f6cbc80efc0794
Pablo Sabater <pabloosabaterr@gmail.com>
2021-01-13 00:10:42 +0100

    feat: getSpotByCategorie

--- src/services/spot/index.ts
+++ src/services/spot/index.ts
@@ -37,3 +37,22 @@ export const getSpotById = async(token: string, id: string) => {
     return res;
   }
 }
+
+export const getSpotByCategories = async(token: string, categorie: string) => {
+  let res;
+  try {
+    const { data, status } = await axios.get(
+      `${process.env.REACT_APP_API_URI}/api/spotcategories/${categorie}`,
+      {
+        headers: { 'auth-token': token },
+      }
+    );
+    res = { data, status };
+  } catch (err) {
+    const { status } = err.response;
+    const { message } = err.response.data;
+    res = { status, message };
+  } finally {
+    return res;
+  }
+}
