b7e9595a95500e2a67c10094b14900c6ddd4612b
Pablo Sabater <pabloosabaterr@gmail.com>
2021-02-01 19:17:20 +0100

    feat: getLikedSpot Service

--- src/services/user/index.ts
+++ src/services/user/index.ts
@@ -14,8 +14,8 @@ export const getLikedPost = async (token: string) => {
     const { status } = err.response;
     const { message } = err.response.data;
     res = { status, message };
-  }finally{
-    console.clear()
+  } finally {
+    console.clear();
     return res;
   }
 };
@@ -34,8 +34,28 @@ export const giveLike = async (spotId: number, token: string) => {
     const { status } = err.response;
     const { message } = err.response.data;
     res = { status, message };
-  }finally{
-    console.clear()
+  } finally {
+    console.clear();
+    return res;
+  }
+};
+
+export const getUserLikedSpots = async (token: string) => {
+  let res;
+  try {
+    const { data, status } = await axios.get(
+      `${process.env.REACT_APP_API_URI}/api/getLiked`,
+      {
+        headers: { 'auth-token': token },
+      }
+    );
+    res = { data, status };
+  } catch (err) {
+    const { status } = err.response;
+    const { message } = err.response.data;
+    res = { status, message };
+  } finally {
+    console.clear();
     return res;
   }
 };
